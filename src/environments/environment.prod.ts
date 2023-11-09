9b3f7250603e297405c9a6ed8abc9c9c26c799b6
AnastasiaBogacheva <trakylenok@gmail.com>
2023-11-09 19:50:03 +0300

    add environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'API key', //your API key here
+  fbDBUrl: 'https://database-link' //your firebase database link
+};
